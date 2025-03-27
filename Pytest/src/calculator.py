46e559b94b89154aa864ef1e4ff6904ff12d27de
matyasmagera <94050118+matyasmagera@users.noreply.github.com>
2025-03-27 08:30:45 +0100

    divide_test

--- Pytest/src/calculator.py
+++ Pytest/src/calculator.py
@@ -12,3 +12,19 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
+
+import math
+
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Cannot take log of non-positive number!")
+    if b <= 0:
+        raise ZeroDivisionError("Cannot take log with non-positive base!")
+    if b == 1:
+        raise NameError("Cannot take log with base 1!")
+    return math.log(a, b)
+
+if __name__ == "__main__":
+    a = float(input("Zadej první číslo: "))
+    b = float(input("Zadej druhé číslo: "))
